64daeb8cbf3775e990d1d8853937eb604da72fb2
Lisa Li <flyingpeach@live.com>
2022-08-11 14:00:39 -0700

    Revert to true rank() calculation

--- tools/get_rank_ratio.m
+++ tools/get_rank_ratio.m
@@ -5,15 +5,4 @@ EPS = 1e-8; % hardcoded
 mtx = get_local_subspace(sys, x0, params, adjustLocality);
 
 fprintf('Calculating rank\n');
-n     = (sys.Nu*(params.tFIR_-1)); % Max possible rank
-svals = svds(mtx, n); % Largest singular values
-
-zeroSv = find(svals < EPS, 1); % = rank+1
-if isempty(zeroSv)
-    rankRatio = 1; % largest n singular values are all nonzero
-else
-    rankRatio = (zeroSv-1)/n;
-end
-
-% For comparison
-% rankRatioFull = rank(full(mtx), EPS) / (sys.Nu*(params.tFIR_-1))
+rankRatio = rank(full(mtx), EPS) / (sys.Nu*(params.tFIR_-1));
