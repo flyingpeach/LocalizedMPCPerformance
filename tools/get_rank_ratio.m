8f5ff1bb856de2d7fdde998d1ad7c4a88b63d9ef
Lisa Li <flyingpeach@live.com>
2022-08-19 10:35:20 -0700

    Update print formatting for rank ratios

--- tools/get_rank_ratio.m
+++ tools/get_rank_ratio.m
@@ -1,7 +1,7 @@
 function rankRatio = get_rank_ratio(sys, x0, params, adjustLocality)
 % adjustLocality: adjust definition of locality to work with grid example
 
-mtx = get_local_subspace(sys, x0, params, adjustLocality);
+mtx       = get_local_subspace(sys, x0, params, adjustLocality);
+rankRatio = rank(full(mtx)) / (sys.Nu*(params.tFIR_-1));
+fprintf('Rank ratio: %.2f\n', rankRatio);
 
-fprintf('Calculating rank\n');
-rankRatio = rank(full(mtx)) / (sys.Nu*(params.tFIR_-1))
